5af258f59dea930d5cef9f33135ff1f2a013dc9d
somnathElvirainfotech <somnath.elvirainfotech@gmail.com>
2023-01-04 19:32:49 +0530

    db config

--- api/config/dbconfig.js
+++ api/config/dbconfig.js
@@ -4,18 +4,52 @@ require("dotenv").config();
 
 
 // database setup --------------------------------------------------------------------------------
-const conn = mysql.createConnection({
+// const conn = mysql.createConnection({
+//     host: process.env.db_host,
+//     user: process.env.db_user,
+//     password: process.env.db_password,
+//     database: process.env.db_name
+//     // multipleStatements:true
+// });
+
+// conn.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected to database');
+// });
+
+
+
+var db_config = {
     host: process.env.db_host,
     user: process.env.db_user,
     password: process.env.db_password,
     database: process.env.db_name
-    // multipleStatements:true
-});
+};
 
-conn.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to database');
-});
+var conn;
+
+function handleDisconnect() {
+  conn = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  conn.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  conn.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 
 
